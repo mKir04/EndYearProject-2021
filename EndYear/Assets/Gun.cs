94934dbd0d27d2e40f0a8b7cae737d2656c225ba
Mirza Kir <46354349+mKir04@users.noreply.github.com>
2021-06-13 02:24:34 -0400

    shooting+gun

--- EndYear/Assets/Gun.cs
+++ EndYear/Assets/Gun.cs
@@ -5,7 +5,9 @@ public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+
     public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
 
     // Update is called once per frame
     void Update()
@@ -16,6 +18,7 @@ public class Gun : MonoBehaviour
     }
 
     void Shoot(){
+        muzzleFlash.Play();
         RaycastHit hit;
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)){
             Debug.Log(hit.transform.name);
